b7736528013380a831da456da91b846e3311fcb2
Viktoria <zaharova181114@gmail.com>
2022-07-11 22:32:49 +0300

    add modal

--- app/js/common.js
+++ app/js/common.js
@@ -76,3 +76,8 @@ $('.projects-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
 });
+
+
+$('[name="phone"]').mask('+7 (999) 99-99-99');
+
+
